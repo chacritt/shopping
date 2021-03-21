5cbe2256325f1602327abaeb504b153ae828c4a6
chacritt <chacrit1853@gmail.com>
2021-03-21 22:17:02 +0700

    db

--- oom_shop.sql
+++ oom_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 23, 2021 at 01:30 AM
+-- Generation Time: Mar 21, 2021 at 03:16 PM
 -- Server version: 5.7.17-log
 -- PHP Version: 5.6.30
 
@@ -19,6 +19,24 @@ SET time_zone = "+00:00";
 --
 -- Database: `oom_shop`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`username`, `password`) VALUES
+('admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -39,9 +57,17 @@ CREATE TABLE `category` (
 
 CREATE TABLE `descript` (
   `id` int(5) NOT NULL,
-  `member_id` int(3) NOT NULL,
-  `item_id` int(5) NOT NULL
+  `item_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `item_price` int(5) NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `descript`
+--
+
+INSERT INTO `descript` (`id`, `item_name`, `item_price`, `address`) VALUES
+(8, 'nike air jordan 1 retro high og', 25900, '63 m.8 t.natai bannaderm suratthani 84240');
 
 -- --------------------------------------------------------
 
@@ -55,15 +81,18 @@ CREATE TABLE `item` (
   `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `descript` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `category_id` int(3) NOT NULL,
-  `member_id` int(3) NOT NULL
+  `shop_id` int(3) NOT NULL,
+  `price` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `item`
 --
 
-INSERT INTO `item` (`id`, `image`, `name`, `descript`, `category_id`, `member_id`) VALUES
-(1, '1.png', 'chairs', 'chairs', 1, 1);
+INSERT INTO `item` (`id`, `image`, `name`, `descript`, `category_id`, `shop_id`, `price`) VALUES
+(1, 'https://www.pngkey.com/png/full/939-9395949_air-jordan-1-retro-high-og-spider-man.png', 'nike air jordan 1 retro high og', 'If Spider-Man was a sneaker head, he probably would be rocking the Jordan 1 Retro High Spider-Man Origin Story. This AJ1 comes with a white upper plus red accents, black Nike Ã¢ÂÂSwooshÃ¢ÂÂ, white midsole, and translucent sole.', 1, 1, 25900),
+(2, 'https://www.pngarts.com/files/8/Nike-Air-Force-One-PNG-Image.png', 'nike air force 1', 'Air Force 1\r\nFrom high-top to low-top. Live the legend of the Air Force 1, modernized for ultimate street style.', 1, 1, 3500),
+(3, 'https://www.pngkey.com/png/full/316-3169214_nike-air-max-97-og-x-undftd-air.png', 'nike air max 97 ', 'nike air max 97 ', 1, 1, 6400);
 
 -- --------------------------------------------------------
 
@@ -94,9 +123,29 @@ INSERT INTO `member` (`id`, `username`, `password`, `name`, `email`, `tel`, `sta
 (7, '', '', '', '', '', 2),
 (8, '', 'aa', '', '', '', 2);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shop`
+--
+
+CREATE TABLE `shop` (
+  `id` int(3) NOT NULL,
+  `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`username`);
 
 --
 -- Indexes for table `category`
@@ -123,6 +172,12 @@ ALTER TABLE `member`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `shop`
+--
+ALTER TABLE `shop`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -135,17 +190,22 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `descript`
 --
 ALTER TABLE `descript`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `shop`
+--
+ALTER TABLE `shop`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
